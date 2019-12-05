c28e4203598942e784eea447f2e2e60fc4983c52
Nipapan Aom <nipapan@maqe.com>
2019-12-05 13:24:12 +0700

    customize bulma

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,10 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 import Msgbox from "../components/msgbox";
-
-import 'bulma/css/bulma.css';
+import './mystyles.scss';
 
 const IndexPage = () => (
   <Layout>
